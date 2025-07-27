df8cf0bfb8b5f3d6f13a450f08f3f66db1401064
aweb99 <132497632+aweb99@users.noreply.github.com>
2025-07-27 15:58:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.get("/test", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("🟢 Puppeteer API running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🟢 Puppeteer API running on port ${PORT}`);
 });
